456a0d07c49099e79379913bfa1b11113bed0bfb
Krzysztof575 <krzysztofryszka2@gmail.com>
2020-07-27 18:32:26 +0200

    1

--- GraZgadywnaka/ConsoleAppMonolitycznie/Program.cs
+++ GraZgadywnaka/ConsoleAppMonolitycznie/Program.cs
@@ -6,7 +6,44 @@ namespace ConsoleAppMonolitycznie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Gra \"Za dużo za mało\"");
+
+            //1. Komputer losuje liczbę do odgadniecia
+            #region komputer suje
+            var los = new Random(); //tworzymy gegrator liczb losowych
+            int wylosowana = los.Next(1, 101);// losujemy liczbę z podanego zakresu
+#if DEBUG           
+            Console.WriteLine(wylosowana);
+#endif           
+            
+            Console.WriteLine("wylosowałem liczb od 1 do 100. + \nOdgadnij ją!");
+            #endregion
+
+            Console.Write("Podaj swoją prpozycję:");
+            //2. Człowiek proponuje wartość
+            int propozycja = int.Parse(Console.ReadLine());
+
+
+            //3. Komputer ocenia propozycję człowieka
+            #region komputer ocenia
+            if (propozycja < wylosowana)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Za mało");
+                Console.ResetColor();
+            }
+            else if (propozycja > wylosowana)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Za dużo");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Trafiałeś");
+                Console.ResetColor();
+            }
         }
     }
 }
